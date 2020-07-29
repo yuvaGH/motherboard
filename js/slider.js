8cc7d2e921831a18266059876458f91ae9360156
yuvaGH <yuvareznor@gmail.com>
2020-07-29 22:02:56 +0300

    layout + get started slider.js

---
+++ js/slider.js
@@ -0,0 +1,25 @@
+let countSlider = null;
+let currentDot = 0;
+let prevDot = 0;
+
+window.onload = function() {
+  document.getElementById('right').addEventListener('click', sliderRight);
+  document.getElementById('left').addEventListener('click', sliderLeft);
+
+  const slider = document.querySelector('.slider-containter');
+  countSlider = slider.childElementCount;
+
+  for (let i = 0; i < countSlider; i++) {
+    slider.children[i].classList.add(`slider-${i + 1}`);
+  }
+
+}
+
+
+function sliderRight() {
+  slider.children[]
+}
+
+function sliderLeft() {
+  console.log('left');
+}
